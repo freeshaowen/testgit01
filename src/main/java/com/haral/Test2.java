efbb58372a22e9c66232f560a2949c262f44cbab
tom_glb <630glb@qq.com>
2020-02-13 18:35:13 +0800

    01分支提交====222

--- src/main/java/com/haral/Test2.java
+++ src/main/java/com/haral/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("01 02写的");
         System.out.println("hot_fix 开发的");
+        System.out.println("hot_fix 再加一行");
     }
 }
