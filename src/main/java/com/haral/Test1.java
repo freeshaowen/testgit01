31599cad439a1a908c4147a0a5fb56032f4c0a39
tom_glb <630glb@qq.com>
2020-02-13 18:55:44 +0800

    hot_fix分支 解决问题

--- src/main/java/com/haral/Test1.java
+++ src/main/java/com/haral/Test1.java
@@ -2,6 +2,7 @@ package com.haral;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("this is the first");
+        System.out.println("this is the first2");
+        System.out.println("this is the first iss53");
     }
 }
--- src/main/java/com/haral/Test2.java
+++ src/main/java/com/haral/Test2.java
@@ -5,5 +5,7 @@ public class Test2 {
         System.out.println("01 02写的");
         System.out.println("hot_fix 开发的");
         System.out.println("hot_fix 再加一行");
+        System.out.println("iss53 添加一行");
+        System.out.println("hot_fix 添加一行");
     }
 }
